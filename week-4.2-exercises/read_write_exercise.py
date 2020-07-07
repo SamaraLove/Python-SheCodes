99c7b25e16b4ed18b3694562323022e1108b21b1
SamaraLove <samaramglove@gmail.com>
2020-07-07 17:59:54 +0800

    Read/write exercise complete
    Completed q2 and q3

--- week-4.2-exercises/read_write_exercise.py
+++ week-4.2-exercises/read_write_exercise.py
@@ -1,6 +1,6 @@
 ########## Question 1 ##########
 
-## Example 1 output 
+## Example 1 output to only the terminal
 # names = []
 # with open("names.txt") as txt_file:
 #     for no,name in enumerate(txt_file, 1):
@@ -31,37 +31,67 @@ with open("names_output.txt", "w" ) as txt_file:
         # print(f"{no}. {name}")
         txt_file.write(name + "\n")
 
+
 ########## Question 2 ##########
+import csv
+
+colours = []
+
+with open("colours_20.csv") as csv_file:
+# with open("colours_213.csv") as csv_file:
+
+    reader = csv.reader(csv_file)
+    # header = next(reader)
+    for row in reader:
+        # For each row it fetched the contents of that row as a list and printed that list.
+        output = row[1], row[2], row[4]
+        colours.append(output)
+print(colours)
+with open("colours_20_output.csv", "w", newline='') as csv_file:
+# with open("colours_213_output.csv", "w", newline='') as csv_file:
+# newline='' removes the empty line after every line
+
+    csv_writer = csv.writer(csv_file, delimiter=",")
+    for item in colours:
+        csv_writer.writerow(item)
 
 
 ########## Question 3 ##########
-
-    
-
 import csv
 
-# colours = []
+colours_English = []
 i = 0
-b =0
 g = 0
+b = 0
+# with open("colours_20.csv") as csv_file:
 with open("colours_213.csv") as csv_file:
-    reader = csv.reader(csv_file, delimiter=",")
-    ##cycling through first row
+
+    reader = csv.reader(csv_file)
+    # header = next(reader)
     for row in reader:
-        # print(row)
-        for column in row:
-            if column == " English Name":
-                print(column)
-                print(row)
-                # print(row)
-                # next(csv_file)
-                next(reader)
-                print(row)
-                if item == "Beige":
-                    i = i + 1
-                elif column =="blue":
-                    b = b + 1
-                elif column == "green":
-                    g = g + 1
-    print(i)
-    print(f"Red: {i}\nGreen: {g}\nBlue: {b}")
+        output = row[4]
+        colours_English.append(output)
+        print(output)
+
+# print(colours_English)
+        if "red" in output:
+            i = i + 1     
+        elif "green" in output:
+            g = g + 1   
+        elif "blue" in output:
+            b = b + 1
+#     ic = colours_English.count("red")
+#     gc = colours_English.count("green")
+#     bc = colours_English.count("blue")
+# print(f"\nRed: {i}\nGreen: {g}\nBlue: {b}\n")
+# print(f"\nRed: {ic}\nGreen: {gc}\nBlue: {bc}\n")
+
+print(len(colours_English))
+# with open("colours_20_output.csv", "w", newline='') as csv_file:
+with open("colours_213_output.csv", "w", newline='') as csv_file:
+# newline='' removes the empty line after every line
+
+    csv_writer = csv.writer(csv_file, delimiter=",")
+    for item in colours_English:
+        # print(item)
+        csv_writer.writerow([item])
